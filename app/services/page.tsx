49b6a8e509c8860072613b9978a629f80438d2c2
Bilal Jadi <148371977+B-JCoder@users.noreply.github.com>
2025-09-08 12:49:54 -0700

    add services page

---
+++ app/services/page.tsx
@@ -0,0 +1,25 @@
+import { Header } from "@/components/header"
+import { HeroSection } from "@/components/hero-section"
+import { AboutSection } from "@/components/about-section"
+import { ServicesSection } from "@/components/services-section"
+// import { PricingSection } from "@/components/pricing-section"
+import { TestimonialsSection } from "@/components/testimonials-section"
+import { PhotoGallery } from "@/components/photo-gallery"
+import { ContactSection } from "@/components/contact-section"
+import { Footer } from "@/components/footer"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <Header />
+      <HeroSection />
+      <AboutSection />
+      <ServicesSection />
+      {/* <PricingSection /> */}
+      <TestimonialsSection />
+      <PhotoGallery />
+      <ContactSection />
+      <Footer />
+    </main>
+  )
+}
